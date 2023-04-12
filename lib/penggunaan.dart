bf38b46c1cd8c6d18c5c1d5582bc74d669798921
syafira widiyanti <syafirawidiyanti11@gmail.com>
2023-04-12 10:46:51 +0700

    add steps of playlist menu

--- lib/penggunaan.dart
+++ lib/penggunaan.dart
@@ -30,7 +30,7 @@ class HelpPage extends StatelessWidget {
             Text(
               'Berikut ini adalah langkah-langkah untuk mengakses menu daftar anggota dalam aplikasi mobile Kelompok 4:\n'
               '1. Pada halaman Menu Utama, klik menu Daftar Anggota \n'
-              '2. Setelah masuk ke halaman menu Daftar Anggota, pengguna dapat melihat gambar, nama, dan nomor mahasiswa dari masing-masing anggota kelompok',
+              '2. Setelah masuk ke halaman menu Daftar Anggota, pengguna dapat melihat gambar, nama, dan nomor mahasiswa dari masing-masing anggota kelompok \n',
             ),
             SizedBox(height: 8.0),
             Text(
@@ -63,7 +63,7 @@ class HelpPage extends StatelessWidget {
             Text(
                 '1. Untuk menjeda stopwatch yang berjalan, ketuk tombol Stop \n'
                 '2. Untuk melanjutkan stopwatch, ketuk tombol Start kembali \n'
-                '3. Untuk mereset stopwatch, ketuk tombol Reset'
+                '3. Untuk mereset stopwatch, ketuk tombol Reset \n'
             ),
             SizedBox(height: 8.0),
             Text(
@@ -73,7 +73,14 @@ class HelpPage extends StatelessWidget {
               ),
             ),
             Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie euismod nulla, sit amet semper mi blandit ac. Suspendisse eget feugiat enim. Nulla facilisi. Duis tincidunt elit nec enim dictum sagittis. Nam commodo commodo tellus sed posuere. Donec id nisi in tellus feugiat consectetur. Nam lacinia ligula nec orci facilisis, sed euismod libero rutrum. Integer sed odio id diam bibendum commodo vel et nunc.',
+              'Berikut ini adalah langkah-langkah untuk mengakses menu playlist dalam aplikasi mobile Kelompok 4: \n'
+              '1. Pada halaman Menu Utama, klik menu Playlist \n'
+              '2. Setelah masuk ke halaman menu Stopwatch, pengguna dapat melihat list dari beberapa lagu yang tersedia \n'
+              '3. Untuk melihat detail lagu, ketuk lagu pada layar \n'
+              '4. Halaman akan berpindah ke halaman detail lagu sesuai lagu yang dipilih \n'
+              '5. Pengguna dapat melihat gambar, judul lagu, nama penyanyi, deskripsi lagu, tanggal rilis, dan genre lagu \n'
+              '6. Untuk mendengarkan lagu, ketuk ikon share di sebelah kanan atas layar \n'
+              '7. Untuk menambahkan lagu ke dalam favorite, ketuk ikon love yang terdapat pada halaman list lagu \n'
             ),
             SizedBox(height: 8.0),
             Text(
